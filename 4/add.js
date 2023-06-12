4274ae25699b5ff9d367510ca267a0c6bb61a875
Della Izadi <delaram.ae@gmail.com>
2023-06-12 14:45:37 +0100

    Adding add function to the program

---
+++ 4/add.js
@@ -0,0 +1,7 @@
+// / Define a function add (in a file add.js) which takes two arguments and return their sum.
+const add = (number, num) => {
+    return number + num;
+  };
+  
+  console.log(add(2, 3));
+  
