8c6e64f4e80f05134d01930eb29e7caabef71870
Della Izadi <delaram.ae@gmail.com>
2023-06-12 14:46:21 +0100

    Adding add multiply function to the program

---
+++ 4/multiply.js
@@ -0,0 +1,7 @@
+// / Define a function add (in a file add.js) which takes two arguments and return their sum.
+const multiply= (number, num) => {
+    return number * num;
+  };
+  
+  console.log(multiply(2, 3));
+  
