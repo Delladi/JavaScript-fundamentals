ad2b9df4e9cec09a2d522ff239afc3411d59efc4
Della Izadi <delaram.ae@gmail.com>
2023-06-13 17:31:13 +0100

    Created a prgram that uses filter function to filter the phone numbers.

---
+++ 11_array_operations/phone_numbers.js
@@ -0,0 +1,25 @@
+const checkLength = (phoneNumber) => {
+    if (phoneNumber.length <= 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const filterLongNumbers = (phoneNumbers) => {
+    return phoneNumbers.filter(checkLength);
+  };
+
+  const numbers = [
+    '1763687364',
+    '4763687363',
+    '7867867862',
+    'aaaaaaaabbbbbbbcccccdddddddd' // this element should be filtered
+  ];
+
+  console.log(filterLongNumbers(numbers));
+
+  console.log(filterLongNumbers(['4763687363', '7867867862']));
+
+  console.log(filterLongNumbers([]));
+
