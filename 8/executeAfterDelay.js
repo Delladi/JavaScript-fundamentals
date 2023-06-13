3b68acbd2c7635174748fec3144577de675c973e
Della Izadi <delaram.ae@gmail.com>
2023-06-13 14:34:40 +0100

    Added delayed function using executeAfterDelay.

---
+++ 8/executeAfterDelay.js
@@ -0,0 +1,11 @@
+const printHello = () => {
+    console.log('Hello!');
+  };
+  
+  const executeAfterDelay = (delay, callBackFunction) => {
+    setTimeout(callBackFunction, delay * 1000);
+  };
+  
+  executeAfterDelay(5, printHello);
+  // After about 5 seconds, you should see the message being logged
+  // Hello!
